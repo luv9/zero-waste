a6f53a9997f97d21ea843ed34e385f83c4ba96aa
iamsahilsoni <sonisahil12345@gmail.com>
2022-10-26 00:07:57 -0400

    DONT MERGE !!, demo PR for understaning

--- zero-waste-ui/src/app/app.module.ts
+++ zero-waste-ui/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './main/dashboard/dashboard.component';
+import { WasteListComponent } from './main/dashboard/waste-list/waste-list.component';
+import { WasteDetailsComponent } from './main/dashboard/waste-details/waste-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    WasteListComponent,
+    WasteDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ zero-waste-ui/src/app/main/dashboard/waste-details/waste-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ListType } from '../waste-list/types';
+
+@Component({
+  selector: 'app-waste-details',
+  templateUrl: './waste-details.component.html',
+  styleUrls: ['./waste-details.component.css']
+})
+export class WasteDetailsComponent implements OnInit {
+
+  constructor() { }
+ @Input()  selectedBin:ListType;
+
+  ngOnChanges(){
+    console.log("i am ONNNNNNNNN !!!!!!");
+  }
+  ngOnInit(): void {
+    console.log("sdfgh");
+  }
+
+}
