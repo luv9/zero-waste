4a85dc8a21fbb22394fa6d6e83c7d7b1db73ed7e
iamsahilsoni <sonisahil12345@gmail.com>
2022-10-27 14:32:49 -0400

    add bar chart with demo data

---
+++ zero-waste-ui/src/app/main/dashboard/waste-list/waste-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WasteListComponent } from './waste-list.component';
+
+describe('WasteListComponent', () => {
+  let component: WasteListComponent;
+  let fixture: ComponentFixture<WasteListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WasteListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WasteListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
