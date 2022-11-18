195074e9a2df2a98224e041651727a8a1a2e5cb2
Chaitanya Jariwala <jariwalc@uwindsor.ca>
2022-11-18 09:53:15 -0500

    accept token in header

--- zero-waste-service/controllers/authJWT.js
+++ zero-waste-service/controllers/authJWT.js
@@ -5,8 +5,9 @@ const config = require("../config/config");
 const verifyToken = (req, res, next) => {
   // console.log("cookie token value " + req.cookies.token)
   //if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-  if (req.cookies.token) {
-    jwt.verify(req.cookies.token, config.secret_key, function (err, decode) {
+    const token = req.cookies?.token ?? req.header('token');
+  if (token) {
+    jwt.verify(token, config.secret_key, function (err, decode) {
       if (err) {
         req.verifiedUser = undefined;
         res.cookie("token", undefined, { httpOnly: true });
